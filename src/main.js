8a05eae45070af91a23260cf3836b864eede9d42
DR-- <dr188888@163.com>
2018-08-29 08:20:11 +0800

    ajax

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+import HeaderTop from './components/HeaderTop/HeaderTop.vue'
+
+// 注册全局组件标签
+Vue.component('HeaderTop', HeaderTop)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  /* components: { App },
+   template: '<App/>'*/
+  render: h => h(App),//映射app
+  router,//设置路由器
+  store
 })
+// 等同于
+/* render: function (createElement) {
+   return createElement(App)   // return '<App/>'
+ } */
---
+++ src/store/state.js
@@ -0,0 +1,11 @@
+/*
+vuex管理的状态对象
+ */
+
+export default {
+  latitude: 34.32038, // 纬度
+  longitude: 112.16867, // 经度
+  address: {}, // 地址信息对象
+  categorys: [], // 分类数组
+  shops: [], //商家数组
+}
